c4506eb8a02bb178dc69f39690a9de90095e7798
abdullah-mn <abdullahabda222@gmail.com>
2025-04-06 18:06:45 +0530

    feat(api): Add route to fetch All cheques

--- backend/routes/cheque.route.js
+++ backend/routes/cheque.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createCheque,
   deleteCheque,
+  fetchAllCheque,
   searchCheque,
   updateCheque,
 } from "../controllers/cheque.controller.js";
@@ -11,6 +12,7 @@ const router = express.Router();
 router.post("/", createCheque);
 router.put("/:id", updateCheque);
 router.delete("/:id", deleteCheque);
+router.get("/", fetchAllCheque);
 
 router.get("/search?", searchCheque);
 
