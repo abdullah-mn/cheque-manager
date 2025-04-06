0d023b16ea26ccabed8736c83efa771805e590a9
abdullah-mn <abdullahabda222@gmail.com>
2025-04-06 18:02:32 +0530

    feat(api): Add route to update an existing cheque

--- backend/routes/cheque.route.js
+++ backend/routes/cheque.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createCheque } from "../controllers/cheque.controller.js";
+import {
+  createCheque,
+  updateCheque,
+} from "../controllers/cheque.controller.js";
 
 const router = express.Router();
 
 router.post("/", createCheque);
+router.put("/:id", updateCheque);
 
 export default router;
