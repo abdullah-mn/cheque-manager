c7eec8ad5076fac5a77a07c0a8e2678b9c2da033
abdullah-mn <abdullahabda222@gmail.com>
2025-04-06 18:03:48 +0530

    feat(api): Add route to delete a cheque

--- backend/routes/cheque.route.js
+++ backend/routes/cheque.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createCheque,
+  deleteCheque,
   updateCheque,
 } from "../controllers/cheque.controller.js";
 
@@ -8,5 +9,6 @@ const router = express.Router();
 
 router.post("/", createCheque);
 router.put("/:id", updateCheque);
+router.delete("/:id", deleteCheque);
 
 export default router;
